5d417ca53c96b8c383012fae40ca50068091aa1a
Stefan <1stefan.wolf1@gmail.com>
2026-07-19 19:52:03 +0200

    updated Layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,80 +2,75 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+        <!-- Navbar title -->
+        <q-toolbar-title class="q-ml-lg"> Test App </q-toolbar-title>
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <!-- Navbar buttons -->
+        <div
+          v-for="headerbutton in headerButtons"
+          :key="headerbutton.name" class="q-mx-sm"
+        >
+          <q-btn
+            :label="headerbutton.name"
+            flat
+            no-caps
+          />
+        </div>
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer >
+      <q-toolbar>
+        <!-- Navbar title -->
+        <div>
+          <p style="margin: 0px;"> {{ "© Test Seite " + year }} </p>
+        </div>
+
+        <div class="col"/>
+        <!-- Navbar buttons -->
+        <div
+          v-for="footerbutton in footerButtons"
+          :key="footerbutton.name"
+          class="q-mx-sm"
+        >
+          <q-btn
+            :label="footerbutton.name"
+            flat
+            no-caps
+          />
+        </div>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
+const headerButtons = [
+  {
+    name: "Objekte"
+  },
+  {
+    name: "About me"
+  },
+  {
+    name: "Kontakt"
+  },
+]
 
-const linksList: EssentialLinkProps[] = [
+const footerButtons = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
+    name: "Datenschutz"
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
+    name: "Impressum"
   },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
-];
+]
 
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
+const dateObject = new Date();
+const year = dateObject.getFullYear()
 </script>
